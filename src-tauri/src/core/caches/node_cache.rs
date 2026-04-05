77899dd79b068341bd5127fb913621e07f11831d
Gavin <50980660+GavinHaydy@users.noreply.github.com>
2026-04-05 21:11:24 +0800

    Fix node domain (#64)
    * feat: extract tar
    
    * fix: fmt err
    
    * fix: node url

--- src-tauri/src/core/caches/node_cache.rs
+++ src-tauri/src/core/caches/node_cache.rs
@@ -23,9 +23,9 @@ enum BoolOrString {
 pub async fn fetch_versions_node() -> Result<Vec<String>, String> {
     let proxy = get_config_bool("proxy", false);
     let url = if proxy {
-        format!("{}/dist/index.json", EDownload::NodeProxy)
+        format!("{}/index.json", EDownload::NodeProxy)
     } else {
-        format!("{}/dist/index.json", EDownload::Node)
+        format!("{}/index.json", EDownload::Node)
     };
 
     let releases: Vec<NodeRelease> = reqwest::get(url)
